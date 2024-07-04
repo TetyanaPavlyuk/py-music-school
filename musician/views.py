7614432fe4572502cd239e70a6d57774509bf945
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-07-04 14:12:18 +0300

    Add id field into serializer

--- musician/views.py
+++ musician/views.py
@@ -4,6 +4,6 @@ from musician.models import Musician
 from musician.serializers import MusicianSerializer
 
 
-class MusicianViewSet(viewsets):
+class MusicianViewSet(viewsets.ModelViewSet):
     queryset = Musician.objects.all()
     serializer_class = MusicianSerializer
