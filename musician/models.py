a11719fcfae41f5c8ecd5d7a665a5d22c5a7d0f7
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-07-04 13:50:23 +0300

    Create model Musician

--- musician/models.py
+++ musician/models.py
@@ -2,4 +2,21 @@ from django.db import models
 
 
 class Musician(models.Model):
-    pass
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    instrument = models.CharField(max_length=63)
+    age = models.IntegerField()
+    date_of_applying = models.DateField(auto_now_add=True)
+
+    @property
+    def is_adult(self) -> bool:
+        return self.age >= 21
+
+    def clean(self):
+        if self.age < 14:
+            raise ValueError({
+                "age": f"Age must be at least 14 years old, not {self.age}"
+            })
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
